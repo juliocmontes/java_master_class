3d795155547bba931bc77365f939cce33e65c268
Julio <juliocmontes.a@outlook.com>
2020-05-08 20:12:09 -0700

    worked on classes and constructors

--- Classes/src/com/company/Main.java
+++ Classes/src/com/company/Main.java
@@ -26,20 +26,34 @@ public class Main {
 //        Point point = new Point();
 //        System.out.println("distance()= " + point.distance());
 
+//
+//        Floor test = new Floor(5, 5);
+//        System.out.println(test.getArea());
+//        Floor test2 = new Floor(0, -1);
+//        System.out.println(test2.getArea());
+//
+//        Carpet carpet = new Carpet(3.5);
+//        Floor floor = new Floor(2.75, 4.0);
+//        Calculator calculator = new Calculator(floor, carpet);
+//        System.out.println("total= " + calculator.getTotalCost());
+//        carpet = new Carpet(1.5);
+//        floor = new Floor(5.4, 4.5);
+//        calculator = new Calculator(floor, carpet);
+//        System.out.println("total= " + calculator.getTotalCost());
 
-        Floor test = new Floor(5, 5);
-        System.out.println(test.getArea());
-        Floor test2 = new Floor(0, -1);
-        System.out.println(test2.getArea());
 
-        Carpet carpet = new Carpet(3.5);
-        Floor floor = new Floor(2.75, 4.0);
-        Calculator calculator = new Calculator(floor, carpet);
-        System.out.println("total= " + calculator.getTotalCost());
-        carpet = new Carpet(1.5);
-        floor = new Floor(5.4, 4.5);
-        calculator = new Calculator(floor, carpet);
-        System.out.println("total= " + calculator.getTotalCost());
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
+
 
 
     }
